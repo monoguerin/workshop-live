056a8f1488220d84eb5ab167dd8a40ee94c2bb75
Cesar Guerrero <cesar.guerrero@morningstar.com>
2019-06-25 09:16:04 -0500

    users resolver

--- server/schema.js
+++ server/schema.js
@@ -6,13 +6,35 @@ const schema = makeExecutableSchema({
       id: Int
       title: String
     }
+
+    type User {
+      id: Int
+      name: String!
+      lastname: String
+      email: String!
+    }
  
     type Query {
       posts: [Post]
+      users: [User]!
     }
   `,
   resolvers: {
     Query: {
+      users: () => [
+        {
+          name: 'Cesar',
+          email: 'mono@zemoga.com',
+        },
+        {
+          name: 'Mono',
+          email: 'cesar@zemoga.com',
+        },
+        {
+          name: 'Leonardo',
+          email: 'guerrero@zemoga.com',
+        },
+      ],
       posts: () => [
         {
           id: 1,
