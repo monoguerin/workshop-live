6f8260e2764080cd8ecd8dfc77be1762f46a4880
Cesar Guerrero <cesar.guerrero@morningstar.com>
2019-06-26 08:17:18 -0500

    test

--- apollo/client.js
+++ apollo/client.js
@@ -1,6 +1,11 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
+import getConfig from 'next/config';
 import config from '../appConfig';
+
+const { publicRuntimeConfig } = getConfig();
+
+console.log('publicRuntimeConfig', publicRuntimeConfig);
 
 const client = new ApolloClient({
   uri: `${config.HOST}/graphql`,
--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,4 @@
-const getConfig = require('next/config');
-
-const { publicRuntimeConfig } = getConfig();
-
 const PORT = process.env.PORT || 3000;
-
-console.log('publicRuntimeConfig', publicRuntimeConfig);
 
 module.exports = {
   PORT,
