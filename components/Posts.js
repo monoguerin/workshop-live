f45dae721585ba5d01db79b1149aead75e6f7f9e
Cesar Guerrero <cesar.guerrero@morningstar.com>
2019-06-26 07:35:14 -0500

    hook component

--- components/Posts.js
+++ components/Posts.js
@@ -1,73 +1,98 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Post from './Post';
 
-class Posts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [
-        {
-          title: 'Post 1',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
-          favorite: false,
-        },
-        {
-          title: 'Post 2',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
-          favorite: false,
-        },
-        {
-          title: 'Post 3',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
-          favorite: false,
-        },
-        {
-          title: 'Post 4',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
-          favorite: false,
-        },
-        {
-          title: 'Post 5',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
-          favorite: false,
-        },
-      ],
-    };
-  }
+const POSTS = [
+  {
+    title: 'Post 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
+    favorite: false,
+  },
+  {
+    title: 'Post 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
+    favorite: false,
+  },
+  {
+    title: 'Post 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
+    favorite: false,
+  },
+  {
+    title: 'Post 4',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
+    favorite: false,
+  },
+  {
+    title: 'Post 5',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida porttitor eros nec imperdiet. Proin ligula augue, molestie non cursus et, tincidunt id velit. Aenean porta elementum tellus scelerisque molestie. Donec nec facilisis nibh. Quisque facilisis scelerisque metus, vitae fringilla sem vehicula id. Vestibulum ac turpis et eros vulputate hendrerit sed vitae dui. Donec vehicula dui in fermentum interdum. Proin facilisis condimentum elit at consequat. Fusce enim dui, tincidunt non orci ut, aliquam rutrum sapien.',
+    favorite: false,
+  },
+];
 
-  removePost(index) {
-    const {
-      posts,
-    } = this.state;
+// class Posts extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       posts: POSTS,
+//     };
+//   }
 
+//   removePost(index) {
+//     const {
+//       posts,
+//     } = this.state;
+
+//     const newPosts = [
+//       ...posts,
+//     ];
+
+//     newPosts.splice(index, 1);
+
+//     this.setState({
+//       posts: [
+//         ...newPosts,
+//       ],
+//     });
+//   }
+
+//   render() {
+//     const {
+//       posts,
+//     } = this.state;
+
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         {posts.map(post => ({
+//           ...post,
+//           onClick: this.removePost.bind(this),
+//         })).map(Post)}
+//       </div>
+//     );
+//   }
+// }
+
+const Posts = () => {
+  const [posts, setPosts] = useState(POSTS);
+  const removePost = useCallback((index) => {
     const newPosts = [
       ...posts,
     ];
 
     newPosts.splice(index, 1);
 
-    this.setState({
-      posts: [
-        ...newPosts,
-      ],
-    });
-  }
+    setPosts(newPosts);
+  }, [posts]);
 
-  render() {
-    const {
-      posts,
-    } = this.state;
-
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        {posts.map(post => ({
-          ...post,
-          onClick: this.removePost.bind(this),
-        })).map(Post)}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      {posts.map(post => ({
+        ...post,
+        onClick: removePost,
+      })).map(Post)}
+    </div>
+  );
+};
 
 export default Posts;
