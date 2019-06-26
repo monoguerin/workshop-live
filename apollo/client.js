9241ba230a8f005c792f16c419ed091dd3cdf05f
Cesar Guerrero <cesar.guerrero@morningstar.com>
2019-06-26 08:20:55 -0500

    runtime next config

--- apollo/client.js
+++ apollo/client.js
@@ -1,14 +1,13 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 import getConfig from 'next/config';
-import config from '../appConfig';
 
 const { publicRuntimeConfig } = getConfig();
 
 console.log('publicRuntimeConfig', publicRuntimeConfig);
 
 const client = new ApolloClient({
-  uri: `${config.HOST}/graphql`,
+  uri: `${publicRuntimeConfig.HOST}/graphql`,
   fetch,
 });
 
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: { // Will be available on both server and client
+    HOST: process.env.HOST,
+  },
+};
